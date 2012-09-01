7c7950e170a013210a5a610c583e8caec581e79b
Tristan Stenner <stenner@psychologie.uni-kiel.de>
2012-09-01 19:44:59 +0200

    Corrected name of field

--- g_showval.m
+++ g_showval.m
@@ -4,4 +4,4 @@ global goose
 p = get(goose.gui.ax_fft2,'CurrentPoint');
 p = round(p(1,1:2));
 
-set(goose.gui.txt_fft2Info, 'String', ['max: ',num2str(max(goose.current.fft2img(:)),'%4.2f'),' (overall: ',num2str(goose.current.fft2Max,'%4.2f'),'), current: ',num2str(goose.current.fft2img(p(2), p(1)),'%4.2f')]);
+set(goose.gui.text_fft2Info, 'String', ['max: ',num2str(max(goose.current.fft2img(:)),'%4.2f'),' (overall: ',num2str(goose.current.fft2Max,'%4.2f'),'), current: ',num2str(goose.current.fft2img(p(2), p(1)),'%4.2f')]);
